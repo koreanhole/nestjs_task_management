364de83c29a546b93439e2fbd2d87b26f2be2c24
SoonHyeong Kwon <koreanhole1@gmail.com>
2021-01-25 00:59:56 +0900

    add class validation, custom pipes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty({
+    message: '비어있으면 안됨',
+  })
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -27,10 +27,17 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    return found;
   }
 
   deleteTaskById(id: string): void {
+    this.getTaskById(id);
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
@@ -48,7 +55,6 @@ export class TasksService {
       description,
       status: TaskStatus.OPEN,
     };
-
     this.tasks.push(task);
     return task;
   }
